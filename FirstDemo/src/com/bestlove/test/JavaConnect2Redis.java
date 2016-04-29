883451a40704b6be50f8c5c0b166fcc86650dff4
think <serhou@163.com>
2016-04-29 09:03:47 +0800

    Java使用redis

---
+++ FirstDemo/src/com/bestlove/test/JavaConnect2Redis.java
@@ -0,0 +1,84 @@
+package com.bestlove.test;
+
+import java.util.List;
+import java.util.Set;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Redis key-value数据库
+ * @author think
+ *
+ */
+
+public class JavaConnect2Redis {
+
+	public static void main(String[] args) {
+		//Connecting to Redis server 远程数据库
+		Jedis jedis = new Jedis("192.168.18.129", 6379, 10000);
+		System.out.println(jedis.ping());
+		System.out.println(jedis.get("foo"));
+		System.out.println(jedis.get("age"));
+		String info = jedis.info();
+		System.out.println(info);
+		//lpush
+		jedis.lpush("tutorial-list", "张三丰");
+		jedis.lpush("tutorial-list", "李元霸");
+		jedis.lpush("tutorial-list", "李香君");
+		jedis.lpush("tutorial-list", "柳如是");
+		List<String> list = jedis.lrange("tutorial-list", 0, 5);
+		for (String string : list) {
+			System.out.println(string);
+		}
+		System.out.println(jedis.lrange("tutoriallist", 0, 6));
+		//查找所有符合模式的key
+		Set set = jedis.keys("t*");
+		for (Object object : set) {
+			System.out.println(object);
+		}
+		//delete list
+		System.out.println(jedis.del("tutorial-list"));
+		//hmset
+		System.out.println(jedis.hgetAll("user:1"));
+		//sadd 
+		System.out.println(jedis.smembers("tutoriallist-list"));
+		//zadd  zset (sorted set: 有序集合)
+		System.out.println(jedis.zrangeByScore("tutoriallist-z", 0, 1000));
+		System.out.println(jedis.set("sex", "男"));
+		System.out.println(jedis.get("sex"));
+		System.out.println(jedis.del("sex"));
+		System.out.println(jedis.get("sex"));
+		System.out.println(jedis.exists("sex"));
+		System.out.println(jedis.exists("foo"));
+		//从当前数据库中随机返回一个key
+		System.out.println(jedis.randomKey());
+		//返回key所存储的值的类型
+		System.out.println(jedis.type("tutoriallist-list"));
+		System.out.println(jedis.type("tutoriallist"));
+		System.out.println(jedis.type("tutoriallist-z"));
+		System.out.println(jedis.type("dbname"));
+		System.out.println(jedis.type("age"));
+		//修改key的名称
+//		System.out.println(jedis.rename("foo", "dbname"));
+		System.out.println(jedis.get("dbname"));
+		
+		//HyperLogLog 是用来做基数统计的算法
+		jedis.pfadd("dbproducts", "redis");
+		jedis.pfadd("dbproducts", "mongodb");
+		jedis.pfadd("dbproducts", "oracle");
+		jedis.pfadd("dbproducts", "mysql");
+		jedis.pfadd("dbproducts", "db2");
+		jedis.pfadd("dbproducts", "pgsql");
+		jedis.pfadd("dbproducts", "noe4j");
+		jedis.pfadd("dbproducts", "SQLite");
+		jedis.pfadd("dbproducts", "SQLServer");
+		System.out.println(jedis.pfcount("dbproducts"));
+		jedis.del("dbproducts");
+		//订阅服务 一个客户端要开通一个频道：SUBSCRIBE redisChat
+		jedis.publish("redisChat", "Hello, Redis!");
+		
+		
+	}
+	
+	
+}
