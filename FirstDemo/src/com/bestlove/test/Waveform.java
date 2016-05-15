86f69712b2b30872011f40d47b3649686e4f580d
think <serhou@163.com>
2016-05-15 17:39:49 +0800

    与Processor做比较

--- FirstDemo/src/com/bestlove/test/Waveform.java
+++ FirstDemo/src/com/bestlove/test/Waveform.java
@@ -3,7 +3,7 @@ package com.bestlove.test;
 /**
  * Filter与Processor具有相同的接口元素，但是因为它并非继承自Processor——
  * 因为Filter类的创建者压根不清楚你想要将它用作Processor——因此你不能将Filter
- * 用于Apply.process()方法，即便这样做可以正常运行(实际编译器会报错)。
+ * 用于Apply.process()方法，即便这样做可以正常运行。
  * 这里主要原因是因为Apply.process()方法和Processor之间的耦合过紧，已经超出了
  * 所需要的程度，这就使得应该复用Apply.process()的代码时，复用却被禁止了。
  * 另外还需要注意的是它们输入和输出都是Waveform。
