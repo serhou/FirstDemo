872dcdc7e156b46debf101775a8c06801dfdfb78
think <serhou@163.com>
2016-05-21 16:16:48 +0800

    数字签名算法-RSA

---
+++ FirstDemo/src/com/bestlove/test/TestRSA.java
@@ -0,0 +1,66 @@
+package com.bestlove.test;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * 数字签名算法-RSA
+ * @author think
+ *
+ */
+
+public class TestRSA {
+
+	public static String src = "我爱你美丽的中国！";
+	
+	public static void main(String[] args) {
+		jdkRSA();
+	}
+	
+	public static void jdkRSA() {
+		try {
+			//1、初始化密钥
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator
+					.getInstance("RSA");
+			keyPairGenerator.initialize(512);
+			KeyPair keyPair = keyPairGenerator.genKeyPair();
+			RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+			RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+			
+			
+			//2、执行签名:用私钥进行签名
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateKey.getEncoded());
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+			Signature signature = Signature.getInstance("MD5withRSA");
+			signature.initSign(privateKey);
+			signature.update(src.getBytes());
+			byte[] result = signature.sign();
+			System.out.println("jdk rsa sign:" + Hex.encodeHexString(result));
+			
+			//3、验证签名:用公钥进行验证
+			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+			keyFactory = KeyFactory.getInstance("RSA");
+			PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+			signature = Signature.getInstance("MD5withRSA");
+			signature.initVerify(publicKey);
+			signature.update(src.getBytes());
+			boolean b = signature.verify(result);
+			System.out.println("验证结果：" + b);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
