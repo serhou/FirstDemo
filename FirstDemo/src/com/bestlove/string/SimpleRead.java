7709683ca15e368af7a6adb0b50a97266380b4ee
think <serhou@163.com>
2016-11-10 22:37:34 +0800

    模拟用户输入：格式化和分割字符串

---
+++ FirstDemo/src/com/bestlove/string/SimpleRead.java
@@ -0,0 +1,44 @@
+package com.bestlove.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * StringReader将String转化为可读流对象，然后用这个对象来构造BufferReader对象
+ * @author think
+ *
+ */
+
+public class SimpleRead {
+
+	public static BufferedReader input = new BufferedReader(
+			new StringReader("Sir Robin of Camelot\n22 1.61803"));
+	
+	public static void main(String[] args) {
+		
+		try {
+			System.out.println("What is your name?");
+			String name = input.readLine();
+			System.out.println(name);
+			System.out.println("How old are you? what is your favorite double?");
+			System.out.println("(input: <age> <double>)");
+			String numbers = input.readLine();
+			System.out.println(numbers);
+			
+			String[] numArray = numbers.split(" ");
+			
+			int age = Integer.parseInt(numArray[0]);
+			double favorite = Double.parseDouble(numArray[1]);
+			
+			System.out.format("Hi %s.\n", name);
+			System.out.format("In 5 years you will be %d.\n", age + 5);
+			System.out.format("My favorite double is %f.\n", favorite / 2);
+			
+		} catch (IOException e) {
+			System.err.println("I/O exception");
+		}
+		
+	}
+	
+}
