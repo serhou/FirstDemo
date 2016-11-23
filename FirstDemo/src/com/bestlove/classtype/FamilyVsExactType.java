f5714a5962adba9afce5ec773da37fde3805e842
think <serhou@163.com>
2016-11-23 21:58:38 +0800

    instanceof与Class的等价性

---
+++ FirstDemo/src/com/bestlove/classtype/FamilyVsExactType.java
@@ -0,0 +1,32 @@
+package com.bestlove.classtype;
+
+/**
+ * instanceof和isInstance()生成的结果完全一样
+ * equals()和== 也一样
+ * instanceof或isInstance()保持了类型的概念，equals()和== 比较实际的Class对象没有考虑继承
+ * @author think
+ *
+ */
+
+public class FamilyVsExactType {
+
+	static void test(Object x) {
+		System.out.println("Testing x of type " + x.getClass());
+		System.out.println("x instanceof Base " + (x instanceof Base));
+		System.out.println("x instanceof Derived " + (x instanceof Derived));
+		System.out.println("Base.isInstance(x) " + Base.class.isInstance(x));
+		System.out.println("Derived.isInstance(x) " + Derived.class.isInstance(x));
+		System.out.println("x.getClass() == Base.class " + (x.getClass() == Base.class));
+		System.out.println("x.getClass() == Derived.class " + (x.getClass() == Derived.class));
+		System.out.println("x.getClass().equals(Base.class) " + x.getClass().equals(Base.class));
+		System.out.println("x.getClass().equals(Derived.class) " + x.getClass().equals(Derived.class));
+	}
+	public static void main(String[] args) {
+		test(new Base());
+		test(new Derived());
+	}
+	
+}
+
+class Base {}
+class Derived extends Base {}
